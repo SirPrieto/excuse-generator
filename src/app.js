74cef9cb1bee5423f40739723972d7028856fa83
SirPrieto <hi@abrahamprieto.com>
2022-01-16 18:39:41 +0000

    Master

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRamdon = Math.floor(Math.random() * who.length);
+  let actionRamdon = Math.floor(Math.random() * action.length);
+  let whatRamdon = Math.floor(Math.random() * what.length);
+  let whenRamdon = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoRamdon] +
+    " " +
+    action[actionRamdon] +
+    " " +
+    what[whatRamdon] +
+    " " +
+    when[whenRamdon]
+  );
+}
+
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
